26b022a8bf5129712825f98686399c283e066266
aecsocket <aecsocket@tutanota.com>
2023-02-19 19:02:49 +0000

    Clean up ANSI renderer

--- glossa-core/src/main/kotlin/io/github/aecsocket/glossa/core/AnsiComponentRenderer.kt
+++ glossa-core/src/main/kotlin/io/github/aecsocket/glossa/core/AnsiComponentRenderer.kt
@@ -10,10 +10,6 @@ import java.util.*
 import kotlin.collections.ArrayList
 import kotlin.collections.HashMap
 
-private const val START = "\u001b["
-private const val END = "m"
-private const val RESET = "0"
-
 // Adapted from https://github.com/KyoriPowered/ansi/blob/trunk/src/main/java/net/kyori/ansi/ColorLevel.java
 
 fun interface ColorLevel {
@@ -89,9 +85,9 @@ class AnsiComponentRenderer(
             }
 
             if (escapeCodes.isNotEmpty()) {
-                result.append(START)
+                result.append("\u001b[")
                     .append(escapeCodes.joinToString(";"))
-                    .append(END)
+                    .append("m")
             }
             colorDelta = null
             decorationDelta.clear()
@@ -140,180 +136,6 @@ class AnsiComponentRenderer(
         appendDeltas()
 
         return result.toString()
-
-//        val styleStack = Stack<Style>()
-//        val nextCodes = ArrayList<String>()
-//
-//        fun currentStyle() = Style.style().apply { builder ->
-//            styleStack.forEach { builder.merge(it) }
-//        }.build()
-//
-//        fun appendCodes() {
-//            if (nextCodes.isNotEmpty()) {
-//                result.append(START)
-//                    .append(nextCodes.joinToString(";"))
-//                    .append(END)
-//            }
-//        }
-//
-//        fun Style.setDecorations() = decorations()
-//            .filter { (_, state) -> state == TextDecoration.State.TRUE }
-//            .map { (decoration) -> decoration }
-//
-//        fun TextDecoration.escape() = when (this) {
-//            TextDecoration.BOLD -> "1"
-//            TextDecoration.ITALIC -> "3"
-//            TextDecoration.UNDERLINED -> "4"
-//            TextDecoration.OBFUSCATED -> "5"
-//            TextDecoration.STRIKETHROUGH -> "9"
-//        }
-//
-//        ComponentFlattener.basic().flatten(input, object : FlattenerListener {
-//            override fun pushStyle(style: Style) {
-//                val oldStyle = currentStyle()
-//                styleStack.push(style)
-//                var color: TextColor? = null
-//                val decorations = HashSet<TextDecoration>()
-//
-//                // the style we just entered...
-//                style.color()?.let {
-//                    if (it != oldStyle.color()) {
-//                        // ...changed the color
-//                        color = it
-//                    }
-//                }
-//                var changedDecorations = false
-//                style.decorations().forEach { (decoration, state) ->
-//                    when (state) {
-//                        TextDecoration.State.TRUE -> {
-//                            // ...added a decoration, so we add it to the escapes
-//                            decorations += decoration
-//                        }
-//                        TextDecoration.State.FALSE -> {
-//                            // ...disabled a decoration, so we must rebuild the current escape from scratch
-//                            changedDecorations = true
-//                        }
-//                        else -> {}
-//                    }
-//                }
-//
-//                nextCodes.clear()
-//                if (changedDecorations) {
-//
-//
-//                    // rebuilding the escape from scratch
-//                    // merge all styles up to this point
-//                    val currentStyle = Style.style().apply { builder ->
-//                        styleStack.forEach { builder.merge(it) }
-//                    }.build()
-//
-//                    // rebuild the escape
-//                    color = currentStyle.color()
-//                    currentStyle.decorations().forEach { (decoration, state) ->
-//                        if (state == TextDecoration.State.TRUE) {
-//                            decorations += decoration
-//                        }
-//                    }
-//                }
-//
-//                color?.let { nextCodes += colorLevel.escape(it.value()) }
-//                decorations.forEach { decoration ->
-//                    nextCodes += when (decoration) {
-//                        TextDecoration.BOLD -> "1"
-//                        TextDecoration.ITALIC -> "3"
-//                        TextDecoration.UNDERLINED -> "4"
-//                        TextDecoration.OBFUSCATED -> "5"
-//                        TextDecoration.STRIKETHROUGH -> "9"
-//                    }
-//                }
-//            }
-//
-//            override fun popStyle(style: Style) {
-//                styleStack.pop()
-//                val currentStyle by lazy { currentStyle() }
-//                var color: TextColor? = null
-//                val decorations = HashSet<TextDecoration>()
-//
-//                // the style we just exited...
-//                style.color()?.let {
-//                    // ...changed the color
-//                    color = currentStyle.color()
-//                }
-//                var changedDecorations  = false
-//                style.decorations().forEach { (_, state) ->
-//                    when (state) {
-//                        TextDecoration.State.TRUE, TextDecoration.State.FALSE -> {
-//                            // ...changed at least one decoration, so we do a full decoration rebuild
-//                            changedDecorations = true
-//                        }
-//                        else -> {}
-//                    }
-//                }
-//
-//                // ...so we build the escape sequence
-//                nextCodes.clear()
-//                if (changedDecorations) {
-//                    // a decoration was changed; do a full rebuild
-//                    nextCodes += RESET
-//                    decorations += currentStyle.setDecorations()
-//                }
-//                color?.let { nextCodes += colorLevel.escape(it.value()) }
-//                decorations.forEach { decoration ->
-//                    nextCodes += decoration.escape()
-//                }
-//            }
-//
-//            override fun component(text: String) {
-//                appendCodes()
-//                result.append(text)
-//            }
-//        })
-//
-//        return result.toString()
-//
-//        ComponentFlattener.basic().flatten(input, object : FlattenerListener {
-//            val styleStack = Stack<Style>()
-//
-//            override fun pushStyle(style: Style) {
-//                styleStack.push(style)
-//            }
-//
-//            override fun component(text: String) {
-//                val styleBuilder = Style.style()
-//                styleStack.forEach { styleBuilder.merge(it) }
-//                val currentStyle = styleBuilder.build()
-//
-//                val ansiCodes = ArrayList<String>()
-//                currentStyle.color()?.let { color ->
-//                    ansiCodes += colorLevel.escape(color.value())
-//                }
-//                currentStyle.decorations().forEach { (decoration, state) ->
-//                    if (state == TextDecoration.State.TRUE) {
-//                        ansiCodes += when (decoration) {
-//                            TextDecoration.BOLD -> "1"
-//                            TextDecoration.ITALIC -> "3"
-//                            TextDecoration.UNDERLINED -> "4"
-//                            TextDecoration.OBFUSCATED -> "5"
-//                            TextDecoration.STRIKETHROUGH -> "9"
-//                            else -> throw IllegalStateException("Invalid decoration $decoration")
-//                        }
-//                    }
-//                }
-//
-//                if (ansiCodes.isNotEmpty()) {
-//                    ansiCodes.add(0, RESET)
-//                    result.append(START)
-//                        .append(ansiCodes.joinToString(";"))
-//                        .append(END)
-//                }
-//                result.append(text)
-//            }
-//
-//            override fun popStyle(style: Style) {
-//                styleStack.pop()
-//            }
-//        })
-//        return "$result$START$RESET$END"
     }
 }
 
